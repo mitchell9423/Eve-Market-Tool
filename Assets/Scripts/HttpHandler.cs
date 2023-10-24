cfb69425df7e7179b1be01d319095d5a4abd3d71
mitchell9423 <37316395+mitchell9423@users.noreply.github.com>
2023-10-24 09:50:23 -0400

    Added HttpHandler class

---
+++ Assets/Scripts/HttpHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace EveMarket
+{
+	public class HttpHandler : MonoBehaviour
+	{
+		private readonly HttpClient _httpClient;
+
+		public HttpHandler()
+		{
+			_httpClient = new HttpClient();
+		}
+
+		public IEnumerator Get<T>(string url, System.Action<string> callback)
+		{
+			using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+			{
+				// Request and wait for the desired page.
+				yield return webRequest.SendWebRequest();
+
+				if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+				{
+					Debug.Log($"Web request Error: {webRequest.error}");
+					callback(null);
+				}
+				else
+				{
+					callback(webRequest.downloadHandler.text);
+				}
+			}
+		}
+	}
+}
