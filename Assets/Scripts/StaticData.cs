887cf6250883f4b78827260fa1434040a0c5ecb2
mitchell9423 <37316395+mitchell9423@users.noreply.github.com>
2023-10-24 09:50:00 -0400

    Added StaticData class

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+namespace EveMarket
+{
+	public static class StaticData
+	{
+		public static Dictionary<int, string> Groups = new Dictionary<int, string>()
+		{
+			{ 512, "Arkonor" },
+			{ 514, "Bistot" },
+			{ 515, "Pyroxeres" },
+			{ 516, "Plagioclase" },
+			{ 517, "Spodumain" },
+			{ 518, "Veldspar" },
+			{ 519, "Scordite" },
+			{ 521, "Crokite" },
+			{ 522, "Dark Ochre" },
+			{ 523, "Kernite" },
+			{ 525, "Gneiss" },
+			{ 526, "Omber" },
+			{ 527, "Hedbergite" },
+			{ 528, "Hemorphite" },
+			{ 529, "Jaspet" },
+			{ 530, "Mercoxit" }
+		};
+	}
+}
