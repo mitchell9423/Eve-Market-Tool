bccd909bc9ec94f7fee8876f3dd17c743a9ca33d
mitchell9423 <37316395+mitchell9423@users.noreply.github.com>
2023-10-24 09:49:33 -0400

    Added DataModel Class

---
+++ Assets/Scripts/IDataModel.cs
@@ -0,0 +1,89 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EveMarket
+{
+	public interface IDataModel
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+	}
+
+	[Serializable]
+	public class MarketGroup : IDataModel
+	{
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		[JsonProperty("group_id")]
+		public int Id { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("parent_group_id")]
+		public int ParentGroupId { get; set; }
+
+		[JsonProperty("types")]
+		public List<int> Types { get; set; }
+	}
+
+	[Serializable]
+	public class ItemInfo : IDataModel
+	{
+		[JsonProperty("capacity")]
+		public double Capacity { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		[JsonProperty("dogma_attributes")]
+		public List<DogmaAttribute> DogmaAttributes { get; set; }
+
+		[JsonProperty("group_id")]
+		public int GroupId { get; set; }
+
+		[JsonProperty("icon_id")]
+		public int IconId { get; set; }
+
+		[JsonProperty("market_group_id")]
+		public int MarketGroupId { get; set; }
+
+		[JsonProperty("mass")]
+		public double MassDouble { get; set; }
+		//public decimal Mass => Convert.ToDecimal(MassDouble);
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("packaged_volume")]
+		public double PackagedVolume { get; set; }
+
+		[JsonProperty("portion_size")]
+		public int PortionSize { get; set; }
+
+		[JsonProperty("published")]
+		public bool Published { get; set; }
+
+		[JsonProperty("radius")]
+		public double Radius { get; set; }
+
+		[JsonProperty("type_id")]
+		public int Id { get; set; }
+
+		[JsonProperty("volume")]
+		public double Volume { get; set; }
+	}
+
+	[Serializable]
+	public class DogmaAttribute
+	{
+		[JsonProperty("attribute_id")]
+		public int AttributeId { get; set; }
+		[JsonProperty("value")]
+		public double Value { get; set; }
+	}
+}
